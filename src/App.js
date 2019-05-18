14908992203e2bc9b6c5bbdf52040b7d8f7ede2b
debbysa <deby.silvia23@gmail.com>
2019-05-18 23:26:40 +0700

    task and timer using socket

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  socket = socketIOClient("http://localhost:3000");
+
+  state = {
+    workshop: {
+      judul: "",
+      deskripsi: ""
+    },
+    task: {
+      start: false,
+      judul: "",
+      durasi: 0
+    }
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/workshop/1")
+      .then(response => this.setState({ workshop: response.data }));
+  }
+
+  toggleStart() {
+    const { task } = this.state;
+    task.start = !task.start;
+    this.setState({ task }, () => {
+      if (this.state.task.start) this.startTimer();
+      else this.stopTimer();
+    });
+  }
+
+  changeJudul(judul) {
+    const { task } = this.state;
+    task.judul = judul;
+    this.setState({ task });
+  }
+
+  changeDurasi(durasi) {
+    const { task } = this.state;
+    task.durasi = durasi;
+    this.setState({ task });
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      if (this.state.task.durasi > 0) {
+        let { task } = this.state;
+        task.durasi--;
+        this.socket.emit("task", task);
+        this.setState({ task });
+      } else {
+        this.toggleStart();
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    const { task } = this.state;
+    task.durasi = 0;
+    this.setState({ task });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.workshop.judul}</h1>
+        <p>{this.state.workshop.deskripsi}</p>
+
+        {this.state.task.start ? (
+          <div>
+            <p align="center">{this.state.task.judul}</p>
+            <p align="center">{this.state.task.durasi}</p>
+            <button onClick={() => this.toggleStart()}>Berhenti</button>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <label>
+                Judul Task
+                <input
+                  type="text"
+                  value={this.state.task.judul}
+                  onChange={event => this.changeJudul(event.target.value)}
+                />
+              </label>
+            </div>
+
+            <div>
+              <label>
+                Durasi Task
+                <input
+                  type="text"
+                  value={this.state.task.durasi}
+                  onChange={event => this.changeDurasi(event.target.value)}
+                />
+              </label>
+            </div>
+            <button onClick={() => this.toggleStart()}>Mulai</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
