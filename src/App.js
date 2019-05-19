0b816d133ee205befb4b2fdb102d9d0fc79b1464
debbysa <deby.silvia23@gmail.com>
2019-05-19 20:08:01 +0700

    adding styled-components halaman pemateri tab tugas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+      padding: 10
+      font-size: 1.5em
+      color: #226b80
+    `;
+const ParagraphId = styled.p`
+  text-align: right;
+  padding: 20px
+  border: 1px solid #aaa;
+`;
+
+const Tab = styled.div`
+    overflow: hidden
+    border: 1px solid #ccc
+    background-color: #f1f1f1
+    `;
+
+const TabButton = styled.button`
+  background-color: inherit
+    float: left
+    border: none
+    cursor: pointer
+    padding: 14px 16px
+    transition: 0.3s
+    font-size: 17px
+    &:hover {
+    background: #226b80;
+    color: white;
+  }
+`;
 
 class App extends Component {
   socket = socketIOClient("http://localhost:3000");
 
   state = {
     workshop: {
+      id_workshop: "",
       judul: "",
       deskripsi: ""
     },
@@ -63,23 +95,31 @@ class App extends Component {
     clearInterval(this.timer);
     const { task } = this.state;
     task.durasi = 0;
+    task.judul = "";
     this.setState({ task });
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.workshop.judul}</h1>
+        <Title>{this.state.workshop.judul}</Title>
         <p>{this.state.workshop.deskripsi}</p>
+        <ParagraphId>{this.state.workshop.id_workshop}</ParagraphId>
+
+        <Tab>
+          <TabButton>Tugas</TabButton>
+          <TabButton>Peserta</TabButton>
+          <TabButton>Percakapan</TabButton>
+        </Tab>
 
         {this.state.task.start ? (
-          <div>
+          <div align="center">
             <p align="center">{this.state.task.judul}</p>
             <p align="center">{this.state.task.durasi}</p>
             <button onClick={() => this.toggleStart()}>Berhenti</button>
           </div>
         ) : (
-          <div>
+          <div align="center">
             <div>
               <label>
                 Judul Task
